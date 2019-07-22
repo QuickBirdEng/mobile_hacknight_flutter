a453a1a4c91301ae76a1bbed616f75696a2c4839
Julian Bissekkou <julianbissekkou@quickbirdstudios.com>
2019-07-22 17:00:17 +0200

    add documentation for tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,75 @@
-
 import 'package:flutter/material.dart';
-import 'package:hacknight_example/demo_app.dart';
+import 'package:hacknight_example/movie_catalog/detail/movie_detail_page.dart';
+import 'package:hacknight_example/movie_catalog/model/models.dart';
 import 'package:hacknight_example/movie_catalog/movie_catalog_app.dart';
+import 'package:hacknight_example/movie_catalog/movie_item.dart';
 
+/// Task1:
+/// - Create a [ListView] and display a list of [MovieItem]s.
+/// - Adjust the style of the list items in the [MovieItem] class.
+///
+///
+///
+/// Hints:
+///
+/// [Image.network] can be used to display the image of the movie.
+/// Example: Image.network(Uri.decodeFull(movie.imageUrl))
+///
+///
+/// Is your text overflowing? Try [Flexible].
+/// Example:
+/// Flexible(
+///   child: Text(
+///     movie.title,
+///     overflow: TextOverflow.ellipsis,
+///   ),
+/// )
+///
+
+/// Task2:
+/// - Use [Navigator.push] to route to the [MovieDetailPage].
+///
+///
+///
+/// Hints:
+///
+/// Create a [MaterialPageRoute] that returns the
+/// instance of [MovieDetailPage] in its builder function.
+///
+/// Example:
+/// MaterialPageRoute(builder: (context) => MovieDetailPage(movie: clickedMovie));
+
+/// Task3:
+/// - Display the information of a [Movie] in the existing [MovieDetailPage] class.
+///
+///
+///
+/// Hints:
+///
+/// You create a simple page skeleton by using a [Scaffold].
+/// Try setting [Scaffold.body] or [Scaffold.appBar] (use [AppBar] for that).
+///
+///
+/// Task4:
+/// Adjust Design accordingly to the design found in
+/// https://dribbble.com/shots/5352058-Movie-Application/attachments
+///
+///
+/// Hints:
+///
+/// Use [Stack] to put widgets on top of each other. This can be used to put a
+/// [Card] on top of the image as shown in the design.
+///
+/// [SingleChildScrollView] can be used to make a bigger widget scrollable.
+///
+/// Create a [Container] and adjust [Container.foregroundDecoration]
+/// to create different gradients on an image.
+/// Example:
+/// foregroundDecoration: BoxDecoration(
+///   gradient: LinearGradient(colors: [
+///     Colors.black26,
+///     Colors.black45,
+///     Colors.black,
+///   ]),
+/// )
 void main() => runApp(MovieCatalogApp());
--- lib/movie_catalog/movie_catalog_app.dart
+++ lib/movie_catalog/movie_catalog_app.dart
@@ -5,21 +5,6 @@ import 'package:hacknight_example/movie_catalog/movie_item.dart';
 
 import 'model/models.dart';
 
-/// Task1:
-/// Create a [ListView] and display a list of [MovieItem]s.
-/// Mocked data is stored in [mockedMovies].
-///
-/// Task2: Adjust the style of the items to display more than just the name
-/// (Optional: Display the image of the movie)
-/// (Optional: Implement search feature by using [TextField] and [TextEditingController])
-/// (Optional: Display the rating using 5 colored stars)
-///
-/// Task3: Use the [Navigator] to route to the [MovieDetailPage]
-/// (Optional: Adjust Design accordingly to the design found in https://dribbble.com/shots/5352058-Movie-Application/attachments)
-///
-/// Task4: Display a list of reviews by implement [ReviewContainer].
-/// (Optional: Make list horizontally scrollable.)
-
 class MovieCatalogApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -61,8 +46,6 @@ class _HomeState extends State<Home> {
   // -----
 
   void _routeToDetailPage(Movie movie) {
-    Navigator.of(context).push<void>(MaterialPageRoute(builder: (context) {
-      return MovieDetailPage(movie: movie);
-    }));
+    Navigator.of(context).push<void>(MaterialPageRoute(builder: (context) => MovieDetailPage(movie: movie)));
   }
 }
