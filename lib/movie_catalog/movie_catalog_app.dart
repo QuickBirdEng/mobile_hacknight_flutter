6522cc931d1a13e1328b26b9460f2acfa82e8419
Julian Bissekkou <julianbissekkou@quickbirdstudios.com>
2019-07-23 11:35:24 +0200

    remove existing ui and create todos

--- lib/movie_catalog/movie_catalog_app.dart
+++ lib/movie_catalog/movie_catalog_app.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:hacknight_example/movie_catalog/detail/movie_detail_page.dart';
-import 'package:hacknight_example/movie_catalog/model/mocked_data.dart';
-import 'package:hacknight_example/movie_catalog/movie_item.dart';
-
-import 'model/models.dart';
 
 class MovieCatalogApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-    );
+    return MaterialApp(home: Home());
   }
 }
 
@@ -24,28 +16,16 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: mockedMovies.length,
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 48),
-        itemBuilder: (context, index) {
-          final movie = mockedMovies[index];
-          return Padding(
-            padding: EdgeInsets.only(bottom: 16),
-            child: MovieItem(
-              movie: movie,
-              onTap: () => _routeToDetailPage(movie),
-            ),
-          );
-        },
+      // TODO replace "Center" with a ListView
+      body: Center(
+        child: Text(
+          "Let's get 💩 done!",
+          style: TextStyle(
+            fontSize: 38,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ),
     );
-  }
-
-  // -----
-  // Routing
-  // -----
-
-  void _routeToDetailPage(Movie movie) {
-    Navigator.of(context).push<void>(MaterialPageRoute(builder: (context) => MovieDetailPage(movie: movie)));
   }
 }
--- lib/movie_catalog/movie_item.dart
+++ lib/movie_catalog/movie_item.dart
@@ -3,103 +3,16 @@ import 'package:flutter/material.dart';
 import 'model/models.dart';
 
 class MovieItem extends StatelessWidget {
-  final VoidCallback onTap;
   final Movie movie;
 
-  MovieItem({@required this.onTap, @required this.movie});
+  MovieItem({@required this.movie});
 
   @override
   Widget build(BuildContext context) {
-    final radius = Radius.circular(16);
-    final borderRadius = BorderRadius.all(radius);
-
-    final border = RoundedRectangleBorder(borderRadius: borderRadius);
-
-    return Card(
-      shape: border,
-      elevation: 12,
-      child: InkWell(
-        onTap: onTap,
-        customBorder: border,
-        child: Container(
-          height: 132,
-          child: Row(
-            children: [
-              ClipRRect(
-                child: Image.network(
-                  Uri.decodeFull(movie.imageUrl),
-                  width: 112,
-                  fit: BoxFit.fitWidth,
-                ),
-                borderRadius: BorderRadius.only(
-                  topLeft: radius,
-                  bottomLeft: radius,
-                ),
-              ),
-              SizedBox(width: 16),
-              Flexible(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Flexible(
-                      child: Text(
-                        movie.title,
-                        style: TextStyle(
-                          color: Colors.black87,
-                          fontSize: 20,
-                        ),
-                        overflow: TextOverflow.fade,
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.star, color: const Color(0xff8594d0)),
-                        SizedBox(width: 8),
-                        Text(
-                          averageRatingOf(movie),
-                          style: TextStyle(
-                            color: const Color(0xff8594d0),
-                            fontSize: 36,
-                          ),
-                        ),
-                        SizedBox(width: 16),
-                        Expanded(
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: Text(
-                              priceOf(movie),
-                              style: TextStyle(
-                                color: Colors.black54,
-                                fontSize: 18,
-                              ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(width: 16),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+    //TODO add more content here
+    return Container(
+      child: Text(movie.title),
+      color: Colors.black12,
     );
   }
-
-  // -----
-  // Helper
-  // -----
-
-  String averageRatingOf(Movie movie) {
-    final sum =
-        movie.reviews.map((review) => review.score).reduce((a, b) => a + b);
-    final average = (sum / movie.reviews.length).roundToDouble();
-    final roundedString = average.toStringAsFixed(2);
-    return roundedString.substring(0, roundedString.length - 1);
-  }
-
-  String priceOf(Movie movie) => "${movie.ticketPrice} €";
 }
